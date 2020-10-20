ca70acbda54d1e4d95ba618ba094f92383570f0a
Volodymyr Kryachko <vladimir.kryachko@gmail.com>
2020-10-20 12:30:23 +0300

    Removed obsoleted code.

--- boardcontrol.cpp
+++ boardcontrol.cpp
@@ -1,3 +1,25 @@
+/*
+
+Copyright (C) 2019-2020 Volodymyr Kryachko
+
+This file is part of ColorLines.
+
+ColorLines is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+ColorLines is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ColorLines; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
 #include "gameboard.h"
 #include "boardcontrol.h"
 
@@ -145,67 +167,5 @@ void BoardControl::handleClicked( AnimatedIconButton *clickedButton) {
     } else {
         gridControl->setButtonAnimation(*clickedButton);
         selectedCell=clickedButton;
-    }
-}
-
-void BoardControl::handleCellClicked( AnimatedIconButton *clickedButton,
-                                      CellButton *unused)
-{
-    if (clickedButton==selectedCell) {
-        selectedCell=nullptr;
-        gridControl->hideAnimation();
-    }
-    else {
-        if (selectedCell != nullptr) {
-            selectedCell->setState(selectedCell->getState());
-        }
-        if (clickedButton->getState() == CellButton::UNOCCUPIED) {
-            if (selectedCell != nullptr) {
-                board->makeDijkstraSearch(
-                    selectedCell->getRow(),
-                    selectedCell->getColumn());
-
-                std::vector<std::pair<int,int>> path;
-                Board::distance_type dist = board->getReversePathTo(
-                    clickedButton->getRow(),
-                    clickedButton->getColumn(),
-                    path);
-
-                if (dist < OCCUPATION_THRESHOLD) {
-                    gridControl->hideAnimation();
-                    int st = selectedCell->getState();
-                    int delay = 0;
-                    path.push_back(std::make_pair(
-                        selectedCell->getRow(), selectedCell->getColumn()));
-                    AnimatedIconButton *path_button = nullptr;
-                    for (std::vector<std::pair<int,int>>::reverse_iterator ri = path.rbegin();
-                        ri != path.rend();
-                        ++ri) {
-                        path_button = gridControl->getCells()[ri->first][ri->second];
-                        path_button->setupAnimation("opacity", 1, 0, 1000,
-                            AnimatedIconButton::UNOCCUPIED);
-                        gridControl->startDelayedAnimation(path_button, st, delay);
-                        delay+=100;
-                    }
-                    connect(path_button, &AnimatedIconButton::animation_finished,
-                        this,
-                        [this, clickedButton, path_button] {
-                            emit moveFinished(BoardInfo::cell_location(
-                                clickedButton->getRow(), clickedButton->getColumn()));
-                            disconnect(path_button, &AnimatedIconButton::animation_finished, nullptr, nullptr);
-                        });
-                    QTimer::singleShot(
-                        delay,
-                        clickedButton,
-                        [clickedButton, st]{ clickedButton->setState(st); });
-                    selectedCell = nullptr;
-                }
-    //            } else {
-    //                clickedButton->setState(Board::getRandom());
-            }
-        } else {
-            gridControl->setButtonAnimation(*clickedButton);
-            selectedCell=clickedButton;
-        }
     }
 }
--- boardcontrol.h
+++ boardcontrol.h
@@ -1,3 +1,25 @@
+/*
+
+Copyright (C) 2019-2020 Volodymyr Kryachko
+
+This file is part of ColorLines.
+
+ColorLines is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+ColorLines is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ColorLines; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef BOARDCONTROL_H
 #define BOARDCONTROL_H
 
@@ -32,7 +54,6 @@ signals:
     void moveFinished(BoardInfo::cell_location);
 
 public slots:
-    void handleCellClicked(AnimatedIconButton *clickedButton, CellButton *selectedCell);
     void handleClicked( AnimatedIconButton *clickedButton);
 
 private:

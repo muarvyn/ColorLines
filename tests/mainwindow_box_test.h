3a86671207633dc2f6ac1e279fda6bf4656007e5
Volodymyr Kryachko <vladimir.kryachko@gmail.com>
2021-10-18 12:26:55 +0300

    Tested original QBoxLayout behaviour.

---
+++ tests/mainwindow_box_test.h
@@ -0,0 +1,48 @@
+/*
+
+Copyright (C) 2021 Volodymyr Kryachko
+
+This file is part of ColorLines.
+
+ColorLines is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+ColorLines is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ColorLines; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef MAINWINDOW_BOX_TEST_H
+#define MAINWINDOW_BOX_TEST_H
+
+#include <QWidget>
+
+class CustomToolButton;
+class QBoxLayout;
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+public slots:
+    void handleButtonClick();
+
+protected:
+    QList<CustomToolButton *> toolbutton_list;
+    QBoxLayout *first_item, *last_item;
+    QBoxLayout *main_layout;
+
+};
+#endif // MAINWINDOW_BOX_TEST_H
---
+++ tests/testBoxLayout.pro
@@ -0,0 +1,24 @@
+QT       += core gui widgets
+TARGET = testBoxLayout
+TEMPLATE = app
+CONFIG += c++11
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    customtoolbutton.cpp \
+    mainwindow_box_test.cpp
+
+HEADERS += \
+    customtoolbutton.h \
+    mainwindow_box_test.h
+
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
